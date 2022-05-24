068e667352083bd3700e7216120af9837a12af83
adelcampo <adelcampo@example.com>
2022-05-24 20:09:55 +0200

    Added arguments to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
+
 #include "printableRocketCOUT.cpp"
 #include "printableRocketPRINTF.cpp"
 
-int main(void)
+void printUsage()
 {
-  ROCKET::PrintableRocketCOUT aPRCOUT(1);
-  aPRCOUT.printTheRocket();
-  ROCKET::PrintableRocketPRINTF aPRPRINTF(5);
-  aPRPRINTF.printTheRocket();
+  std::cerr << "Usage: rocket -l (1...100) [-i]" << std::endl;
+}
 
+int main(int argc, char *argv[])
+{
+  int retval = 0;
 
-  ROCKET::PrintableRocketPRINTF aPRPRINTFUD(1);
-  aPRPRINTFUD.printTheRocketUpsideDown();
+  if (argc == 3 &&
+      std::string(argv[1]) == "-l" &&
+      strtol(argv[2], nullptr, 0) > 0 &&
+      strtol(argv[2], nullptr, 0) < 101)
+  {
+    ROCKET::PrintableRocketCOUT aPRCOUT(strtol(argv[2], nullptr, 0));
+    aPRCOUT.printTheRocket();
+  }
+  else if (argc == 4 &&
+           std::string(argv[1]) == "-l" &&
+           strtol(argv[2], nullptr, 0) > 0 &&
+           std::string(argv[3]) == "-i")
+  {
+    ROCKET::PrintableRocketPRINTF aPRPRINTF(strtol(argv[2], nullptr, 0));
+    aPRPRINTF.printTheRocketUpsideDown();
+  }
+  else
+  {
+    retval = 1;
+    printUsage();
+  }
 
-  ROCKET::PrintableRocketCOUT aPRCOUTUD(5);
-  aPRCOUTUD.printTheRocketUpsideDown();
-
-
-
-  return 0;
+  return retval;
 }
